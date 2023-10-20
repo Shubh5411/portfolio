c4ff272f2a0cebcd5d236b9cc4a9767188b92e45
shubham <salvishubham000@gmail.com>
2023-10-20 14:59:23 +0530

    Title page created

---
+++ portfolio_master/src/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div className="title animate-charcter">
+            <ReactRotatingText className='title_heading' items={['Full-Stack Developer', 'Jr. React Developer', 'Ui/Ux Designer']} />
+            <h4 >I design and code beautifully simple things, and I love what I do.</h4>
+    </div>
+  
+  )
+}
+
+export default Title
